a401a8b6d74193bd3e69bd94e74aa73b3feee571
marouanedbibih <dbibih.marouane@emsi-edu.ma>
2023-04-27 07:08:51 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let icon = document.querySelector("#icon-nav");
+
+
+icon.onclick = function() {
+    let navbar = document.querySelector(".navigation");
+    navbar.classList.toggle("show");
+}
+
+
+let sendBtn = document.getElementById("send-btn");
+
+var sendEmail = () =>{
+    var paramt = () =>{
+        let email = document.getElementById("email").value;
+        let nom = document.getElementById("name").value;
+        let phone = document.getElementById("phone").value;
+        let message = document.getElementById("message").value;       
+    }
+    const serviceID = "service_85zqy0k";
+
+}
+
+
